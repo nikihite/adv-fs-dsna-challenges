642d7375c9d38de4fd492abfbdc2a534e75837cb
Niki Hite <nikihite@comcast.net>
2022-09-22 10:38:33 -0700

    check syntax test passing

--- day-02/Stack.js
+++ day-02/Stack.js
@@ -17,6 +17,32 @@ class Stack {
     return this.#list[this.#list.length - 1];
   }
 
+  checkSyntax(code) { 
+    const arrayToCheck = new Stack();
+    const opening = [];
+    const closing = [];
+    String(code).split('').map(char => {
+      if (char === '(' ||  char === ')' || char === '{' || char === '}') {
+        arrayToCheck.classPush(char);
+      }
+    });
+    const length = arrayToCheck.#list.length;
+
+    for (let i = 0; i < length; i++) {
+      if (arrayToCheck.classPeek() === '(' || arrayToCheck.classPeek() === '{') {
+        opening.push(1);
+        arrayToCheck.classPop();
+      } else if (arrayToCheck.classPeek() === ')' || arrayToCheck.classPeek() === '}') {
+        closing.push(1);
+        arrayToCheck.classPop();
+      }
+
+    }
+    return String(opening) === String(closing) ? true : false;
+
+  }
+
+
   get readableArr() {
     return this.#list;
   }
